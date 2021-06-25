e3a96186c4588c0e2f9651fb39a90874422134db
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2021-06-25 15:21:58 +0800

    Root exception wrapped in ExecutionException

--- src/main/java/com/kensbunker/java/concurrency/advanced/PlayingWithCallablesAndFutures.java
+++ src/main/java/com/kensbunker/java/concurrency/advanced/PlayingWithCallablesAndFutures.java
@@ -13,8 +13,9 @@ public class PlayingWithCallablesAndFutures {
   public static void main(String[] args)
       throws ExecutionException, InterruptedException, TimeoutException {
     Callable<String> task = () -> {
-      Thread.sleep(300);
-      return "I am in thread " + Thread.currentThread().getName();
+//      Thread.sleep(300);
+//      return "I am in thread " + Thread.currentThread().getName();
+      throw new IllegalStateException("I throw an exception in thread " + Thread.currentThread().getName());
     };
 
     ExecutorService executor = Executors.newFixedThreadPool(4);
