e7ad37c393dcb254f2c999f5aed25ded9abf6ab3
kmandawe <kdmandawe06@yahoo.com.ph>
2021-06-26 17:12:45 +0800

    Using timeout to release threads

--- src/main/java/com/kensbunker/java/concurrency/advanced/BarrierInAction.java
+++ src/main/java/com/kensbunker/java/concurrency/advanced/BarrierInAction.java
@@ -22,13 +22,13 @@ public class BarrierInAction {
         Thread.sleep(random.nextInt(5) * 1000 + 100);
         System.out.println("I just arrived, waiting for the others...");
 
-        barrier.await();
+        barrier.await(5, TimeUnit.SECONDS);
         System.out.println("Let's go to the cinema!");
         return "ok";
       }
     }
 
-    ExecutorService executorService = Executors.newFixedThreadPool(4);
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
     CyclicBarrier barrier = new CyclicBarrier(4, () -> System.out.println("Barrier opening"));
     List<Future<String>> futures = new ArrayList<>();
 
