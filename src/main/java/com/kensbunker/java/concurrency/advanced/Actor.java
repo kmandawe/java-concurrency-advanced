6b9e863544312a001dd99dd057e21411049e5336
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2021-06-30 17:40:48 +0800

    Parallel reduce using ConcurrentHashMap

---
+++ src/main/java/com/kensbunker/java/concurrency/advanced/Actor.java
@@ -0,0 +1,45 @@
+package com.kensbunker.java.concurrency.advanced;
+
+import java.util.Objects;
+
+public class Actor {
+
+  private String lastName, firstName;
+
+  public Actor(String lastName, String firstName) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+  }
+
+  public String lastName() {
+    return this.lastName;
+  }
+
+  public String firstName() {
+    return this.firstName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Actor actor = (Actor) o;
+
+    if (!Objects.equals(lastName, actor.lastName)) {
+      return false;
+    }
+    return Objects.equals(firstName, actor.firstName);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = lastName != null ? lastName.hashCode() : 0;
+    result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+    return result;
+  }
+}
---
+++ src/main/java/com/kensbunker/java/concurrency/advanced/ConcurrentHashMapParallelPatterns.java
@@ -0,0 +1,19 @@
+package com.kensbunker.java.concurrency.advanced;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapParallelPatterns {
+
+  public static void main(String[] args) {
+    ConcurrentHashMap<Actor, Set<Movie>> map = new ConcurrentHashMap<>();
+
+    MovieReader reader = new MovieReader();
+    reader.addActorsToMap(map);
+
+    System.out.println("# Actors = " + map.size());
+
+    int maxMoviesForOneActor = map.reduce(10, (actor, movies) -> movies.size(), Integer::max);
+    System.out.println("Max movies for one actor = " + maxMoviesForOneActor);
+  }
+}
